ba2b8967ffcb3e8ee46c377edde02d39bc1a6c61
Vinay Devarakonda <99051059+vinaenae@users.noreply.github.com>
2026-09-06 14:43:55 -0500

    Add: remove-node-from-end-of-linked-list - submission-20

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-20.py
@@ -0,0 +1,72 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        left = ListNode()
+        right = head
+        left.next = head
+        dummy = left
+        
+        for i in range(n):
+            right = right.next
+
+        while right:
+            right = right.next
+            left = left.next
+        
+        left.next = left.next.next
+        return dummy.next
+        
+
+        # curr = head
+        # start = head
+        # first = head
+        # count = 0
+        # while curr:
+        #     curr = curr.next
+        #     count += 1
+        # remove_elem = (count - n)
+        # loop_node = 0
+        # while start:
+        #     if count == 1:
+        #         first = None
+        #         break
+        #     elif (remove_elem - 1) == -1:
+        #         first = start.next
+        #         break
+        #     else:
+        #         if loop_node == remove_elem - 1:
+        #             start.next = start.next.next
+        #             break
+        #         start = start.next
+        #         loop_node += 1
+        # return first
+        
+        # curr = head
+        # arr = []
+        # while curr:
+        #     arr.append(curr)
+        #     curr = curr.next
+        # start = 1
+        # for i in range(len(arr) - 1, -1, -1):
+        #     if start == n:
+        #         del arr[i]
+        #         break
+        #     start += 1
+        # for i in range(len(arr)):
+        #     if i + 1 < len(arr):
+        #         arr[i].next = arr[i+1]
+        #     else:
+        #         arr[i].next = None
+        # if arr:
+        #     return arr[0]
+        # else:
+        #     return curr
+        
+
+        
+        
